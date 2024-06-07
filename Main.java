e830c097cd0418e8952ba88007f3228b93f9e159
dorsa rezaei <dorsarezaei24@gmail.com>
2024-06-07 18:52:34 +0330

    main of server
    the main code of server project

--- Main.java
+++ Main.java
@@ -1,23 +1,28 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        AtomicInteger clientCounter = new AtomicInteger(0);
+        ArrayList<PrintWriter> clients =new ArrayList<>();
         try {
-            System.out.println("Connected to the server");
-            Socket socket = new Socket("localhost", 1234);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            Scanner scanner=new Scanner(System.in);
-            new Thread(new multiThread(in)).start();
-            while (scanner.hasNextLine()) {
-            out.println(scanner.nextLine());
+            System.out.println("Chat Server is running on port 1234");
+            serverSocket = new ServerSocket(1234);
+            while (true){
+
+                Socket socket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                clients.add(out);
+                String clientName="User" + clientCounter.incrementAndGet();
+                Thread thread=new Thread(new multiThread(socket,clientName,clients));
+                thread.start();
             }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
